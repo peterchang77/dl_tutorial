8dc0fa82e09e633ff7f385fc76f05b863e819941
Ubuntu <ubuntu@ip-10-0-255-248.us-west-2.compute.internal>
2018-04-16 22:36:11 +0000

    updates

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 wget http://169.254.169.254/latest/meta-data/public-ipv4 -O public-ipv4
 source activate dl_aws 
 jupyter notebook --no-browser &
-sleep 1 
+sleep 3 
 jupyter notebook list > token
 python ./print_address.py
